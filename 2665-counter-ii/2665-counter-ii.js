17be2c13956e1e47a35f6b2380b6082635770f69
Aditya Naroliya <96744492+adiX002@users.noreply.github.com>
2023-06-03 00:09:42 +0530

    Time: 69 ms (49.56%), Space: 44.3 MB (90.57%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = (init) => {
+    let counter = init;
+    const increment = () => ++counter;
+    
+    const decrement = () => --counter;
+    
+    const reset = () => {
+        counter = init;
+        return counter;
+    }
+    
+    return {increment, decrement, reset}
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
