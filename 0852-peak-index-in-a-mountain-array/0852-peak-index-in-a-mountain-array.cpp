5b6c860ad8b75e2c6fc686a9680e737fd2b67098
Aditya Naroliya <96744492+adiX002@users.noreply.github.com>
2023-09-14 02:23:38 +0530

    Time: 90 ms (79.97%), Space: 60 MB (13.01%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        //binary search
+        int low = 0;
+        int up = A.size()-1;
+        int cur = A.size()/2;
+        // cout << low << " " << up << " " << cur << endl;
+        while(true){
+        // cout << low << " " << up << " " << cur << endl;
+            if(A[cur-1] < A[cur] && A[cur] < A[cur+1]){ //left side of mountain
+                low = cur;
+                cur = (cur+up)/2;
+            }else if(A[cur-1] > A[cur] && A[cur] > A[cur+1]){ //right side
+                up = cur;
+                cur = (cur+low)/2;
+            }else{
+                return cur;
+            }
+        }
+    }
+};
